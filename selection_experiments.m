67665490d0f7370e508923d9d77b1f8fb9dc3401
Kamal Ranaweera <ranaweer@ualberta.ca>
2018-01-29 17:09:35 -0700

    Selection Experimental Scrip
    A script for experimenting with selection statements.

---
+++ selection_experiments.m
@@ -0,0 +1,14 @@
+% This script contains some experimental code segments used in class
+% to demonstrate the functionality of selection statements.
+
+
+clear
+clc
+
+
+schoolGrade = input('Which grade are you in: ');
+
+if schoolGrade >= 10
+    disp('Yay, you are in High School')
+end
+
